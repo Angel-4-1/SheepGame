89453ed027c899936d57dd6886952b4e5c3100cb
Angel-4-1 <angel.vicente01@estudiant.upf.edu>
2020-04-22 11:34:43 +0200

    Complete

---
+++ Assets/RW/Scripts/Managers/GameStateManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateManager : MonoBehaviour
+{
+    public static GameStateManager Instance;
+
+    [HideInInspector] // To not show the variable assigned to in the editor
+    public int sheepSaved;
+
+    [HideInInspector]
+    public int sheepDropped;
+
+    public int sheepDroppedBeforeGameOver;
+    public SheepSpawner sheepSpawner;
+
+    void Awake()
+    {
+        Instance = this;
+        sheepDropped = 0;
+        sheepSaved = 0;
+    }
+
+    public void SavedSheep()
+    {
+        sheepSaved++;
+        UIManager.Instance.UpdateSheepSaved();
+    }
+
+    private void GameOver()
+    {
+        // Stop the sheep spawner
+        sheepSpawner.canSpawn = false; 
+        sheepSpawner.DestroyAllSheep();
+        // Game Over text
+        UIManager.Instance.ShowGameOverWindow();
+    }
+
+    public void DroppedSheep()
+    {
+        sheepDropped++;
+        UIManager.Instance.UpdateSheepDropped();
+
+        if (sheepDropped == sheepDroppedBeforeGameOver)
+        {
+            GameOver();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Title");
+        }
+    }
+}
---
+++ Assets/RW/Scripts/Sheep.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour
+{
+    public float runSpeed;
+    public float gotHayDestroyDelay;
+    private bool hitByHay;
+
+    private bool isDrop;
+
+    public float dropDestroyDelay; 
+    private Collider myCollider; 
+    private Rigidbody myRigidbody;
+
+    // Sheep Spawner
+    private SheepSpawner sheepSpawner;
+
+    // Heart
+    public float heartOffset;   // Where the heart will spawn (y axis)
+    public GameObject heartPrefab;
+
+    public void SetSpawner(SheepSpawner spawner)
+    {
+        sheepSpawner = spawner;
+    }
+
+    // When sheep drops from the ground
+    private void Drop()
+    {
+        // Sound sheep falling
+        SoundManager.Instance.PlaySheepDroppedClip();
+        // A sheep was dropped
+        GameStateManager.Instance.DroppedSheep();
+        sheepSpawner.RemoveSheepFromList(gameObject);
+        myRigidbody.isKinematic = false; 
+        myCollider.isTrigger = false; 
+        Destroy(gameObject, dropDestroyDelay); 
+    }
+
+    // Sheep has been hitted
+    private void HitByHay()
+    {
+        // A sheep was saved
+        GameStateManager.Instance.SavedSheep();
+
+        //Play song  of sheep hitted
+        SoundManager.Instance.PlaySheepHitClip();
+
+        //Destroy the sheep
+        sheepSpawner.RemoveSheepFromList(gameObject);
+        hitByHay = true;
+        runSpeed = 0;
+        Destroy(gameObject, gotHayDestroyDelay);
+
+        // Create a heart where the sheep died
+        Instantiate(heartPrefab, transform.position + new Vector3(0, heartOffset, 0), Quaternion.identity);
+        // Animation of the heart
+        TweenScale tweenScale = gameObject.AddComponent<TweenScale>(); ; 
+        tweenScale.targetScale = 0; 
+        tweenScale.timeToReachTarget = gotHayDestroyDelay;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hay") && !hitByHay)
+        {
+            Destroy(other.gameObject);
+            HitByHay();
+        }
+        else if (other.CompareTag("DropSheep") && !isDrop)
+        {
+            isDrop = true;
+            Drop();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCollider = GetComponent<Collider>();
+        myRigidbody = GetComponent<Rigidbody>();
+        isDrop = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * runSpeed * Time.deltaTime);
+    }
+}
